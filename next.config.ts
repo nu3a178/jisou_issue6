4d36fc9aa909115979910b71040ed770e05bee3a
nu3a178 <nu197ma@gmail.com>
2026-03-14 20:09:58 +0900

    configファイルの修正をもとに戻した

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value:
-              "https://jisou-issue6-9i054t0qo-nu3a178s-projects.vercel.app/",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
