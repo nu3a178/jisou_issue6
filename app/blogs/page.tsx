745bafd663d7334eb9163abfabbd79b435051ab0
nu3a178 <nu197ma@gmail.com>
2026-03-15 18:14:03 +0900

    ブログ一覧を追加

---
+++ app/blogs/page.tsx
@@ -0,0 +1,26 @@
+import { Card } from "@/components/components/card";
+import { Blog } from "@/types/blog";
+
+export default async function Home() {
+  const apiURl =
+    process.env.NODE_ENV === "production"
+      ? `https://${process.env.DEPLOY_DOMAIN}/api/blog`
+      : `http://localhost:3000/api/blog`;
+  const blogUrl =
+    process.env.NODE_ENV === "production"
+      ? `https://${process.env.DEPLOY_DOMAIN}/blog`
+      : `http://localhost:3000/blog`;
+  const data = (await fetch(apiURl).then((res) => res.json())) as Blog[];
+  return (
+    <div className="flex flex-row flex-wrap gap-y-4 gap-x-4  items-start content-start justify-center  ">
+      {data.map((item, index) => (
+        <Card
+          key={index}
+          title={item.title}
+          description={item.description}
+          url={`${blogUrl}/${item.id}`}
+        />
+      ))}
+    </div>
+  );
+}
